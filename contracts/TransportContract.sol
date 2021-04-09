e7b2f49ba3388433c4b67d5d7ef661f7bac56304
BrianCottrell <Brian.Cottrell.mobile@gmail.com>
2021-04-08 21:11:36 -0700

    Update contract

--- contracts/TransportContract.sol
+++ contracts/TransportContract.sol
@@ -1,10 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.6;
+pragma solidity ^0.6.7;
 
-import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-// https://github.com/pappas999/Parametric-Crop-Insurance/blob/main/contracts/Crop-Insurance.sol
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-contracts/src/v0.6/ChainlinkClient.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/solc-0.6/contracts/access/Ownable.sol";
 
 /**
  * @title TransportContract is an example contract which requests data from
@@ -12,162 +9,66 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @dev This contract is designed to work on multiple networks, including
  * local test networks
  */
-contract TransportContract is ChainlinkClient, Ownable {
 
-  event contractCreated(address _insuranceContract, uint _premium, uint _totalCover);
-
-    /**
-     * @dev Create a new contract for client, automatically approved and deployed to the blockchain
-     */
-    function newContract(address _client, uint _duration, uint _premium, uint _payoutValue, string _cropLocation) public payable onlyOwner() returns(address) {
-
-
-        //create contract, send payout amount so contract is fully funded plus a small buffer
-        InsuranceContract i = (new InsuranceContract).value((_payoutValue * 1 ether).div(uint(getLatestPrice())))(_client, _duration, _premium, _payoutValue, _cropLocation, LINK_KOVAN,ORACLE_PAYMENT);
-
-        contracts[address(i)] = i;  //store insurance contract in contracts Map
-
-        //emit an event to say the contract has been created and funded
-        emit contractCreated(address(i), msg.value, _payoutValue);
-
-        //now that contract has been created, we need to fund it with enough LINK tokens to fulfil 1 Oracle request per day, with a small buffer added
-        LinkTokenInterface link = LinkTokenInterface(i.getChainlinkToken());
-        link.transfer(address(i), ((_duration.div(DAY_IN_SECONDS)) + 2) * ORACLE_PAYMENT.mul(2));
-
-
-        return address(i);
-
-    }
-
+contract GeoDBChainlink is ChainlinkClient, Ownable {
+    uint256 oraclePayment;
+    uint public price = 0;
+    address oracle = 0x56dd6586DB0D08c6Ce7B2f2805af28616E082455;
+    bytes32 jobId = 0x00000000000000000000000000000000ef0e16c96ce04795b261725db827ba32;
+    string radius = "150";
 
     /**
-     * @dev returns the contract for a given address
+     * Network: Kovan
+     * Oracle: 
+     *      Name:           LinkPool
+     *      Listing URL:    https://market.link/nodes/323602b9-3831-4f8d-a66b-3fb7531649eb?network=42&start=1614864673&end=1615469473
+     *      Address:        0x56dd6586DB0D08c6Ce7B2f2805af28616E082455
+     * Job: 
+     *      Name:           GeoDB
+     *      Listing URL:    https://market.link/jobs/...
+     *      ID:             ef0e16c96ce04795b261725db827ba32
+     *      Fee:            0.1 LINK
      */
-    function getContract(address _contract) external view returns (InsuranceContract) {
-        return contracts[_contract];
-    }
 
-
-
-
-
-  uint256 public data;
-
-  /**
-   * @notice Deploy the contract with a specified address for the LINK
-   * and Oracle contract addresses
-   * @dev Sets the storage for the specified addresses
-   * @param _link The address of the LINK token contract
-   */
-  constructor(address _link) public {
-    if (_link == address(0)) {
+    constructor(uint256 _oraclePayment) public {
       setPublicChainlinkToken();
-    } else {
-      setChainlinkToken(_link);
+      oraclePayment = _oraclePayment;
     }
-  }
 
-  /**
-   * @notice Returns the address of the LINK token
-   * @dev This is the public implementation for chainlinkTokenAddress, which is
-   * an internal method of the ChainlinkClient contract
-   */
-  function getChainlinkToken() public view returns (address) {
-    return chainlinkTokenAddress();
-  }
+    function requestUsers
+    (
+      string memory _lat,
+      string memory _lng,
+    )
+      public
+      onlyOwner
+    {
+      Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+      req.add("lat", _lat);
+      req.add("lng", _lng);
+      req.add("radius", radius);
+      req.add("start", "2021-02-07 20:00:00");
+      req.add("end", "2021-04-07 20:00:00");
+      sendChainlinkRequestTo(oracle, req, oraclePayment);
+    }
 
-  /**
-   * @notice Creates a request to the specified Oracle contract address
-   * @dev This function ignores the stored Oracle contract address and
-   * will instead send the request to the address specified
-   * @param _oracle The Oracle contract address to send the request to
-   * @param _jobId The bytes32 JobID to be executed
-   * @param _url The URL to fetch data from
-   * @param _path The dot-delimited path to parse of the response
-   * @param _times The number to multiply the result by
-   */
-  function createRequestTo(
-    address _oracle,
-    bytes32 _jobId,
-    uint256 _payment,
-    string memory _url,
-    string memory _path,
-    int256 _times
-  )
-    public
-    onlyOwner
-    returns (bytes32 requestId)
-  {
-    Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
-    req.add("url", _url);
-    req.add("path", _path);
-    req.addInt("times", _times);
-    requestId = sendChainlinkRequestTo(_oracle, req, _payment);
-  }
+    function requestPrice
+    (
+      string[] memory _locations,
+    )
+      public
+      onlyOwner
+    {
+      price = 0;
+      for (uint i = 0; i < _locations.length; i += 2) {
+        requestUsers(_locations[i], _locations[i + 1]);
+      }
+    }
 
-
-// https://docs.chain.link/docs/geodb-oracle-node#config
-  function requestUsers
-(
-  address _oracle,
-  bytes32 _jobId,
-  string _lat,
-  string _lng,
-  string _radius,
-  string _start,
-  string _end
-)
-  public
-  onlyOwner
-{
-  Chainlink.Request memory req = buildChainlinkRequest(_jobId, this, this.fulfill.selector);
-  req.add("lat", _lat);
-  req.add("lng", _lng);
-  req.add("radius", _radius);
-  req.add("start", _start);
-  req.add("end", _end);
-  sendChainlinkRequestTo(_oracle, req, oraclePayment);
-}
-
-  /**
-   * @notice The fulfill method from requests created by this contract
-   * @dev The recordChainlinkFulfillment protects this function from being called
-   * by anyone other than the oracle address that the request was sent to
-   * @param _requestId The ID that was generated for the request
-   * @param _data The answer provided by the oracle
-   */
-  function fulfill(bytes32 _requestId, uint256 _data)
-    public
-    recordChainlinkFulfillment(_requestId)
-  {
-    data = _data;
-  }
-
-  /**
-   * @notice Allows the owner to withdraw any LINK balance on the contract
-   */
-  function withdrawLink() public onlyOwner {
-    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-    require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
-  }
-
-  /**
-   * @notice Call this method if no response is received within 5 minutes
-   * @param _requestId The ID that was generated for the request to cancel
-   * @param _payment The payment specified for the request to cancel
-   * @param _callbackFunctionId The bytes4 callback function ID specified for
-   * the request to cancel
-   * @param _expiration The expiration generated for the request to cancel
-   */
-  function cancelRequest(
-    bytes32 _requestId,
-    uint256 _payment,
-    bytes4 _callbackFunctionId,
-    uint256 _expiration
-  )
-    public
-    onlyOwner
-  {
-    cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
-  }
+    function fulfill(bytes32 _requestId, uint256 _users)
+      public
+      recordChainlinkFulfillment(_requestId)
+    {
+      price += (100 + _users);
+    }
 }
