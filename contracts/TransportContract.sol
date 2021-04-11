ce4426b14886a785520324a5bb579222a0f33604
BrianCottrell <Brian.Cottrell.mobile@gmail.com>
2021-04-11 11:37:16 -0700

    Change start and end input on contract

--- contracts/TransportContract.sol
+++ contracts/TransportContract.sol
@@ -5,16 +5,18 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/solc-0.6/contracts/access/Ownable.sol";
 
 /**
- * @title TransportContract is an example contract which requests data from
+ * @title TransportContract requests data from
  * the Chainlink network
  * @dev This contract is designed to work on multiple networks, including
  * local test networks
- * ["45.7905","11.9202"]
+ * test location: ["45.7905","11.9202"]
+ * test start: "2021-02-07 20:00:00"
+ * test end: "2021-04-07 20:00:00"
  */
 
 contract GeoDBChainlink is ChainlinkClient, Ownable {
     uint256 oraclePayment;
-    uint256 public price = 150;
+    uint256 public price = 0;
     address oracle = 0x56dd6586DB0D08c6Ce7B2f2805af28616E082455;
     bytes32 jobId = 0x00000000000000000000000000000000ef0e16c96ce04795b261725db827ba32;
     string radius = "150";
@@ -25,6 +27,7 @@ contract GeoDBChainlink is ChainlinkClient, Ownable {
     /**
      * Network: Kovan
      * Oracle: 
+
      *      Name:           LinkPool
      *      Listing URL:    https://market.link/nodes/323602b9-3831-4f8d-a66b-3fb7531649eb?network=42&start=1614864673&end=1615469473
      *      Address:        0x56dd6586DB0D08c6Ce7B2f2805af28616E082455
@@ -40,7 +43,7 @@ contract GeoDBChainlink is ChainlinkClient, Ownable {
       oraclePayment = _oraclePayment;
     }
 
-    function requestUsers(string memory _lat, string memory _lng)
+    function requestUsers(string memory _lat, string memory _lng, string memory _start, string memory _end)
       private
       onlyOwner
     {
@@ -49,28 +52,26 @@ contract GeoDBChainlink is ChainlinkClient, Ownable {
       req.add("lat", _lat);
       req.add("lng", _lng);
       req.add("radius", radius);
-      req.add("start", "2021-02-07 20:00:00");
-      req.add("end", "2021-04-07 20:00:00");
+      req.add("start", _start);
+      req.add("end", _end);
       sendChainlinkRequestTo(oracle, req, oraclePayment);
     }
 
-    function requestPrice(string[] memory _locations)
+    function requestPrice(string[] memory _locations, string memory _start, string memory _end)
       public
-      onlyOwner
     {
       price = 0;
       for (uint i = 0; i < _locations.length; i += 2) {
-        requestUsers(_locations[i], _locations[i + 1]);
+        requestUsers(_locations[i], _locations[i + 1], );
       }
     }
 
     function purchasePass()
       public
       payable
-      onlyOwner
       returns(bool)
     {
-      if (msg.value >= price) {
+      if (1000 * msg.value >= price ether) {
         address payable owner = payable(owner());
         owner.transfer(msg.value);
         return true;
